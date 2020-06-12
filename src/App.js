fe54320aac208998f9897e7d5cfc523ef3e751c4
Claudinei Bido <bido.c@live.com>
2020-06-12 08:41:44 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      todos: [],
+    };
+  }
 
+  handleChange =(e) =>{
+    this.setState({ input: e.target.value });
+  }
+
+  handleClick = () => {
+    const arr = this.state.todos.slice();
+    arr.push(this.state.input);
+    this.setState(() => ({ todos: arr }));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {/* header adding a new item */}
+        <div className="header-add">
+          <input
+            className="form-control"
+            type="text"
+            onChange={this.handleChange}
+            placeholder="Type something..."
+          ></input>
+          <button onClick={this.handleClick} className="btn btn-default btn-lg">
+            <i className="fas fa-plus"></i>
+          </button>
+        </div>
+
+        {/* generate the list of items */}
+        <ul className="list-items">
+          {this.state.todos.map((todo, index) => (
+            <li
+              className={index % 2 === 0 ? 'item-even' : 'item-odd'}
+              key={index}
+            >
+              {`${index + 1} - ${todo}`}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 export default App;
