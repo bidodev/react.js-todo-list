fb9bd9929574f6fb5022a1db98127f5bbde4b0c7
Claudinei Bido <bido.c@live.com>
2020-06-13 13:39:59 +0200

    Updates

---
+++ precache-manifest.591659cde0d2265ef2c8394a369cc926.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ea58375b234207488a5295200f77c5",
+    "url": "/react.js-todo-list/index.html"
+  },
+  {
+    "revision": "397cd74710cb838d39b0",
+    "url": "/react.js-todo-list/static/css/main.9a9d77fb.chunk.css"
+  },
+  {
+    "revision": "f5d270c34600b200a263",
+    "url": "/react.js-todo-list/static/js/2.99981ccb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react.js-todo-list/static/js/2.99981ccb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397cd74710cb838d39b0",
+    "url": "/react.js-todo-list/static/js/main.5a07c263.chunk.js"
+  },
+  {
+    "revision": "0cc0f2ccaa04086a6413",
+    "url": "/react.js-todo-list/static/js/runtime-main.47acdba4.js"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "/react.js-todo-list/static/media/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "/react.js-todo-list/static/media/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "/react.js-todo-list/static/media/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "/react.js-todo-list/static/media/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "/react.js-todo-list/static/media/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "/react.js-todo-list/static/media/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "/react.js-todo-list/static/media/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "/react.js-todo-list/static/media/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "/react.js-todo-list/static/media/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "/react.js-todo-list/static/media/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "/react.js-todo-list/static/media/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "/react.js-todo-list/static/media/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "/react.js-todo-list/static/media/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "/react.js-todo-list/static/media/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "/react.js-todo-list/static/media/fa-solid-900.ec763292.svg"
+  }
+]);
